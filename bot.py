d8d427665beb47db7e7965003bca3c9dc2ecb9df
majid1tejaratbank-commits <majid1tejaratbank@gmail.com>
2026-07-06 08:45:40 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,29 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
+
+from config import TOKEN
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "سلام 🌹\n"
+        "به سامانه آرشیو دیجیتال بردسکن خوش آمدید."
+    )
+
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot Started...")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
